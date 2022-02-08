6d282bc96dcb29e2e06721896069b1edbb2a7014
jcelisca <celiss1997@gmail.com>
2022-02-08 10:18:18 -0500

    CreacÃión de las funciones principales (objetos) con sus atributos

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 (function(){
     self.Board = function(width, height){
         this.width = width;
@@ -8,6 +9,11 @@
         this.ball = null;
         this.count = 0;
     }
+
+    self.board.prototype = {
+
+    }
+
 })();
 
 (function(){
@@ -19,6 +25,54 @@
         this.ctx = canvas.getContext("2d");
     }
 
+    self.board_view.prototype = {
+
+    }
+
+    function draw(ctx, element){
+
+    }
+
+})();
+
+(function(){
+    self.Ball = function(x, y, radius, board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.board = board;
+        this.speed_y = 0;
+        this.speed_x = 2;
+        this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 2;
+        this.speed = 2;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+
+    self.Ball.prototype = {
+
+    }
+
+})();
+
+(function(){
+    self.Bar = function(x, y, width, height, board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 30;
+    }
+
+    self.Bar.prototype = {
+        
+    }
 })();
 
 var board = new Board(700,400);
